948780ad941f11d81ac0aff82115e224714af987
theodarcho <theodarcho@gmail.com>
2026-01-21 18:45:17 +0200

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -35,4 +35,11 @@ def get_request(endpoint, **kwargs):
 # Add code for retrieving sentiments
 
 # def post_review(data_dict):
-# Add code for posting review
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
