840f3542ac71dae0f2661cd47864bda2da5e5302
theodarcho <theodarcho@gmail.com>
2026-01-21 18:33:39 +0200

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for dealer reviews view
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
     # path for add a review view
 
