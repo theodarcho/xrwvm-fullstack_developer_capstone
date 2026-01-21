31a2020ce8f5b084a7cb81e0a94fd6776c232d3f
theodarcho <theodarcho@gmail.com>
2026-01-21 03:45:55 +0200

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
     # path for login
     # path(route='login', view=views.login_user, name='login'),
-     path(route='login', view=views.login_user, name='login'),
+    path(route='login', view=views.login_user, name='login'),
 
     # path for dealer reviews view
 
